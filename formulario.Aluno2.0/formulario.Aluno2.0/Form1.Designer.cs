12bd0ab204546f57657c05c04dbae7457122b52d
Victor Huggo <167533745+ictor226@users.noreply.github.com>
2024-12-03 10:37:26 -0300

    Add files via upload

---
+++ formulario.Aluno2.0/formulario.Aluno2.0/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace formulario.Aluno2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button_Mostrar1 = new System.Windows.Forms.Button();
+            this.button_Apagar1 = new System.Windows.Forms.Button();
+            this.button_ApagarTudo = new System.Windows.Forms.Button();
+            this.button_Mostrar2 = new System.Windows.Forms.Button();
+            this.button_apagar2 = new System.Windows.Forms.Button();
+            this.button_mostrarTudo = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(2, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(286, 213);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(648, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(332, 223);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // button_Mostrar1
+            // 
+            this.button_Mostrar1.Location = new System.Drawing.Point(12, 240);
+            this.button_Mostrar1.Name = "button_Mostrar1";
+            this.button_Mostrar1.Size = new System.Drawing.Size(99, 42);
+            this.button_Mostrar1.TabIndex = 2;
+            this.button_Mostrar1.Text = "mostrar img 1";
+            this.button_Mostrar1.UseVisualStyleBackColor = true;
+            this.button_Mostrar1.Click += new System.EventHandler(this.button_Mostrar1_Click);
+            // 
+            // button_Apagar1
+            // 
+            this.button_Apagar1.Location = new System.Drawing.Point(152, 240);
+            this.button_Apagar1.Name = "button_Apagar1";
+            this.button_Apagar1.Size = new System.Drawing.Size(99, 42);
+            this.button_Apagar1.TabIndex = 3;
+            this.button_Apagar1.Text = "apagar img 1";
+            this.button_Apagar1.UseVisualStyleBackColor = true;
+            this.button_Apagar1.Click += new System.EventHandler(this.button_Apagar1_Click);
+            // 
+            // button_ApagarTudo
+            // 
+            this.button_ApagarTudo.Location = new System.Drawing.Point(451, 318);
+            this.button_ApagarTudo.Name = "button_ApagarTudo";
+            this.button_ApagarTudo.Size = new System.Drawing.Size(99, 42);
+            this.button_ApagarTudo.TabIndex = 4;
+            this.button_ApagarTudo.Text = "apagar as duas img";
+            this.button_ApagarTudo.UseVisualStyleBackColor = true;
+            this.button_ApagarTudo.Click += new System.EventHandler(this.button_ApagarTudo_Click);
+            // 
+            // button_Mostrar2
+            // 
+            this.button_Mostrar2.Location = new System.Drawing.Point(701, 240);
+            this.button_Mostrar2.Name = "button_Mostrar2";
+            this.button_Mostrar2.Size = new System.Drawing.Size(99, 42);
+            this.button_Mostrar2.TabIndex = 5;
+            this.button_Mostrar2.Text = "mostrar img 2";
+            this.button_Mostrar2.UseVisualStyleBackColor = true;
+            this.button_Mostrar2.Click += new System.EventHandler(this.button_Mostrar2_Click);
+            // 
+            // button_apagar2
+            // 
+            this.button_apagar2.Location = new System.Drawing.Point(825, 240);
+            this.button_apagar2.Name = "button_apagar2";
+            this.button_apagar2.Size = new System.Drawing.Size(99, 42);
+            this.button_apagar2.TabIndex = 6;
+            this.button_apagar2.Text = "apagar img 2";
+            this.button_apagar2.UseVisualStyleBackColor = true;
+            this.button_apagar2.Click += new System.EventHandler(this.button_apagar2_Click);
+            // 
+            // button_mostrarTudo
+            // 
+            this.button_mostrarTudo.Location = new System.Drawing.Point(451, 255);
+            this.button_mostrarTudo.Name = "button_mostrarTudo";
+            this.button_mostrarTudo.Size = new System.Drawing.Size(99, 42);
+            this.button_mostrarTudo.TabIndex = 7;
+            this.button_mostrarTudo.Text = "mostrar as duas";
+            this.button_mostrarTudo.UseVisualStyleBackColor = true;
+            this.button_mostrarTudo.Click += new System.EventHandler(this.button_mostrarTudo_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(324, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 42);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "stretch imagem 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(484, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 42);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "stretch imagem 2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(324, 116);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 42);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "zoom imagem 1";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button_mostrarTudo);
+            this.Controls.Add(this.button_apagar2);
+            this.Controls.Add(this.button_Mostrar2);
+            this.Controls.Add(this.button_ApagarTudo);
+            this.Controls.Add(this.button_Apagar1);
+            this.Controls.Add(this.button_Mostrar1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "trabalhando com img";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button_Mostrar1;
+        private System.Windows.Forms.Button button_Apagar1;
+        private System.Windows.Forms.Button button_ApagarTudo;
+        private System.Windows.Forms.Button button_Mostrar2;
+        private System.Windows.Forms.Button button_apagar2;
+        private System.Windows.Forms.Button button_mostrarTudo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ formulario.Aluno2.0/formulario.Aluno2.0/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formulario.Aluno2._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Mostrar1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = true;
+        }
+
+        private void button_Mostrar2_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = true;
+        }
+
+        private void button_Apagar1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image.Dispose();
+            pictureBox1.Image = null;
+        }
+
+        private void button_apagar2_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image.Dispose();
+            pictureBox2.Image = null;
+        }
+
+        private void button_ApagarTudo_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image.Dispose();
+            pictureBox1.Image = null;
+            pictureBox2.Image.Dispose();
+            pictureBox2.Image = null;
+        }
+
+        private void button_mostrarTudo_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = true;
+            pictureBox1.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
